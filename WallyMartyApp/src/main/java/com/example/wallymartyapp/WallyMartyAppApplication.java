0b84c96d4e46b495b0ddc229bc048ed269485a1f
HIDRI A <agthidri24@yahoo.com>
2022-10-25 10:52:50 -0500

    this commit will add the first

---
+++ WallyMartyApp/src/main/java/com/example/wallymartyapp/WallyMartyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallymartyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallyMartyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallyMartyAppApplication.class, args);
+	}
+
+}
---
+++ WallyMartyApp/src/test/java/com/example/wallymartyapp/WallyMartyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallymartyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallyMartyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
